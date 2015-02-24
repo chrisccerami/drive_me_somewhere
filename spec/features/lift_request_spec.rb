2ec29fa5a5fb217479a4001ac63c94a07f57f181
Chris C Cerami <chrisccerami@gmail.com>
2015-02-24 12:58:31 -0500

    Add test for invalid editing of lift request

--- spec/features/lift_request_spec.rb
+++ spec/features/lift_request_spec.rb
@@ -1,56 +1,69 @@
 require "rails_helper"
 
 feature "Lift Request" do
-  it "creates a request with valid attributes" do
-    user = FactoryGirl.create(:user)
-    sign_in(user)
-    visit new_lift_request_path
+  context "creating a request" do
+    before(:each) do
+      user = FactoryGirl.create(:user)
+      sign_in(user)
+      visit new_lift_request_path
+    end
 
-    fill_in "Origin Longitude", with: 42.349932
-    fill_in "Origin Latitude", with: -71.163295
-    fill_in "Origin Address", with: "18 Fairbanks St"
-    fill_in "Origin City", with: "Boston"
-    fill_in "Origin State", with: "MA"
-    fill_in "Origin Zip Code", with: "02135"
+    it "with valid attributes" do
+      fill_in "Origin Longitude", with: 42.349932
+      fill_in "Origin Latitude", with: -71.163295
+      fill_in "Origin Address", with: "18 Fairbanks St"
+      fill_in "Origin City", with: "Boston"
+      fill_in "Origin State", with: "MA"
+      fill_in "Origin Zip Code", with: "02135"
 
-    fill_in "Destination Longitude", with: 40.834362
-    fill_in "Destination Latitude", with: -74.162015
-    fill_in "Destination Address", with: "66 Kenzel Ave"
-    fill_in "Destination City", with: "Nutley"
-    fill_in "Destination State", with: "NJ"
-    fill_in "Destination Zip Code", with: "07110"
+      fill_in "Destination Longitude", with: 40.834362
+      fill_in "Destination Latitude", with: -74.162015
+      fill_in "Destination Address", with: "66 Kenzel Ave"
+      fill_in "Destination City", with: "Nutley"
+      fill_in "Destination State", with: "NJ"
+      fill_in "Destination Zip Code", with: "07110"
 
-    click_on "Request a Ride"
+      click_on "Request a Ride"
 
-    expect(page).to have_content "Request created"
-    expect(page).to have_content "18 Fairbanks St"
-    expect(page).to have_content "66 Kenzel Ave"
+      expect(page).to have_content "Request created"
+      expect(page).to have_content "18 Fairbanks St"
+      expect(page).to have_content "66 Kenzel Ave"
+    end
+
+    it "with invalid attributes" do
+      click_on "Request a Ride"
+
+      expect(page).to have_content "can't be blank"
+      expect(page).to have_content "is invalid"
+    end
   end
 
-  it "doesn't create a request with invalid attributes" do
-    user = FactoryGirl.create(:user)
-    sign_in(user)
-    visit new_lift_request_path
+  context "editing a request" do
+    before(:each) do
+      user = FactoryGirl.create(:user)
+      sign_in(user)
+      lift_request = FactoryGirl.create(:lift_request, user: user)
 
-    click_on "Request a Ride"
+      visit edit_lift_request_path(lift_request)
+    end
 
-    expect(page).to have_content "is invalid"
-  end
+    it "with valid attributes" do
+      fill_in "Destination Address", with: "33 Harrison St"
+      fill_in "Destination City", with: "Boston"
+      fill_in "Destination State", with: "MA"
+      fill_in "Destination Zip Code", with: "02112"
+      click_on "Update Lift Request"
 
-  it "edits a request when valid" do
-    user = FactoryGirl.create(:user)
-    sign_in(user)
-    lift_request = FactoryGirl.create(:lift_request, user: user)
+      expect(page).to have_content "Request updated"
+      expect(page).to have_content "33 Harrison St"
+    end
 
-    visit edit_lift_request_path(lift_request)
+    it "with invalid attributes" do
+      fill_in "Destination Address", with: ""
+      click_on "Update Lift Request"
 
-    fill_in "Destination Address", with: "33 Harrison St"
-    fill_in "Destination City", with: "Boston"
-    fill_in "Destination State", with: "MA"
-    fill_in "Destination Zip Code", with: "02112"
-    click_on "Update Lift Request"
-
-    expect(page).to have_content "Request updated"
-    expect(page).to have_content "33 Harrison St"
+      expect(page).to have_content "can't be blank"
+      expect(page).to have_content "is invalid"
+    end
   end
 end
