f2f1fb8489bc3d15cf397e87f8529b895b6d3004
Chris C Cerami <chrisccerami@gmail.com>
2015-03-02 19:18:12 -0500

    Users can apply to become drivers

---
+++ app/controllers/drivers_controller.rb
@@ -0,0 +1,27 @@
+class DriversController < ApplicationController
+  def new
+    @driver = Driver.new
+    @car = @driver.cars.build
+  end
+
+  def create
+    @driver = current_user.build_driver(driver_params)
+    @car = @driver.cars.build(car_params)
+    if @driver.save && @car.save
+      flash[:notice] = "Application submitted"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def driver_params
+    params.require(:driver).permit(:license_number)
+  end
+
+  def car_params
+    params.require(:car).permit(:make, :model, :color, :year, :license_plate)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :lift_requests, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  resources :drivers, only: [:new, :create]
+
   root to: "lift_requests#index"
 end
